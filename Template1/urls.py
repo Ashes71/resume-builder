8fb2d269be083e38813ca1a38f56515e1e0a933e
dhatrinath <68982920+dhatrinath@users.noreply.github.com>
2021-06-22 12:26:18 +0530

    Update urls.py

--- Template1/urls.py
+++ Template1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 urlpatterns=[
     path('Template1',views.Template1,name='template'),
@@ -7,3 +8,6 @@ urlpatterns=[
     path('Template1/Template1_Contact_Us', views.Template1_Contact_Us, name='Contact_Us')
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
